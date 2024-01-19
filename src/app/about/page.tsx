9d157a5590412a42eca7f44b62c9e7baa2dcb360
Chris Bovard <info@chrisbovard.com>
2024-01-19 11:36:27 -0700

    Fixing the directory; adding in meta data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { BlitzPage } from "@blitzjs/next";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "baaa",
+  description: "boo",
+};
+
 const AboutPage: BlitzPage = () => {
   return <div>This is the about page.</div>;
 };
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog Landing Page",
+  description: "Blog here",
+};
+
 const BlogPage = () => {
   return <div>This is the blog landing page.</div>;
 };
