96dd37fd6352fd6eb45418af610dc30e0e938c47
Chris Bovard <info@chrisbovard.com>
2024-09-15 15:45:30 -0600

    Reverting login to main login and fixing the git ignore

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,16 @@
-import { LoginForm } from "../components/LoginForm";
 import { Title, Stack } from "@mantine/core";
 import { AuthenticationForm } from "../components/MainAuthenticationForm/index";
+// import { LoginForm } from "../components/LoginForm";
 
 export default function LoginPage() {
-  return <LoginForm />;
-  // return (
-  //   <>
-  //     <Stack align="stretch" justify="flex-start" gap="md">
-  //       <Title>Login</Title>
-  //       <Stack align="center" justify="center" style={{ height: "100%" }}>
-  //         <AuthenticationForm />
-  //       </Stack>
-  //     </Stack>
-  //   </>
-  // );
+  return (
+    <>
+      <Stack align="stretch" justify="flex-start" gap="md">
+        <Title>Login</Title>
+        <Stack align="center" justify="center" style={{ height: "100%" }}>
+          <AuthenticationForm />
+        </Stack>
+      </Stack>
+    </>
+  );
 }
